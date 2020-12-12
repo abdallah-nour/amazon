59374cdf8bf9dfa4c2629489dfd50bded3743e65
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-12-12 23:33:53 +0200

    #19
    search page finished-2

---
+++ src/components/SearchResultCard/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import * as S from './style';
+import * as Txt from '../Txt';
+import Button from '../Button';
+import { useStateValue } from '../StateProvider';
+
+
+export default function SearchResultCard({ headerContent, imgSrc, sponsoredTxt, descTxt, isReviewed = true, nmbStars, nmbReviews, currentPrice, prevPrice, shipTxt, nmbStock }) {
+  const [{ cart }, dispatch] = useStateValue();
+  const addToCart = () => {
+    dispatch({
+      type: 'ADD_TO_CART',
+      item: {
+        image: imgSrc,
+        title: descTxt,
+        price: currentPrice,
+      }
+    })
+  }
+
+  return (
+    <S.Card>
+      <S.CardHeader >{headerContent}</S.CardHeader>
+      <Txt.CustomLink >
+        <S.CardImg src={imgSrc} />
+      </Txt.CustomLink>
+      {sponsoredTxt && <S.Sponsor></S.Sponsor>}
+      <div>
+        <S.DescTxt color='#0F1111' size='1rem' height='24px' lines='4' >
+          {descTxt}
+        </S.DescTxt>
+      </div>
+      {<S.Reviews nmbStars={nmbStars} nmbReviews={nmbReviews} />}
+      {<S.Price>{currentPrice}</S.Price>}
+      <Txt.P color='#565959' margin='3px 0 0 0'>{shipTxt}</Txt.P>
+      <Button onClick={addToCart} position='absolute' bottom='8px' width='50%' height='25px' >Add to Cart</Button>
+    </S.Card>
+  )
+}
