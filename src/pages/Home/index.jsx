846ce88fbba36a12b6255e6968ea9e0006bae1ac
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-12-12 23:22:27 +0200

    #18
    finish side menu

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,30 @@
+import { useState } from 'react';
 import MainSlider from '../../components/MainSlider';
 import CardContainer from '../../components/CardContainer';
 import ProductSlider from '../../components/ProductSlider';
 import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import SideMenu from '../../components/SideMenu';
 import * as S from './style'
 
 function Home() {
+  const [visible, setVisible] = useState(false);
   return (
-    <S.Home>
-      <MainSlider />
-      <CardContainer />
-      <ProductSlider headerTxt='Discover Amazon' headerLink='Click to learn more' defaultArrowBtnDisable={true} >
-        <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Projects/HelpPage/BubbleShoveler/English/Fuji_Bubble_8Languages_en_US_updated_1x._CB445837675_.png' alt='' />
-        <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_Currency_en_US_1X._CB451228332_.jpg' alt='' />
-        <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_SecurePayment_en_US_1X._CB451228335_.jpg' alt='' />
-        <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_ImportFees_en_US_1X._CB451228332_.jpg' alt='' />
-        <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_TrackPackage_en_US_1X._CB451228335_.jpg' alt='' />
-        <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_247CS_en_US_1X._CB451228332_.jpg' alt='' />
-      </ProductSlider>
-      {/* <Link to='/'><img src='' alt='' /></Link> */}
-      <Footer />
-    </S.Home>
+      <S.Home>
+        <SideMenu visible={visible} setVisible={setVisible} />
+        <Header setDarkScreen={setVisible} />
+        <MainSlider />
+        <CardContainer />
+        <ProductSlider headerTxt='Discover Amazon' headerLink='Click to learn more' defaultArrowBtnDisable={true} >
+          <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Projects/HelpPage/BubbleShoveler/English/Fuji_Bubble_8Languages_en_US_updated_1x._CB445837675_.png' alt='' />
+          <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_Currency_en_US_1X._CB451228332_.jpg' alt='' />
+          <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_SecurePayment_en_US_1X._CB451228335_.jpg' alt='' />
+          <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_ImportFees_en_US_1X._CB451228332_.jpg' alt='' />
+          <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_TrackPackage_en_US_1X._CB451228335_.jpg' alt='' />
+          <img src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/October/BubbleShoveler/AIS_Bubble_247CS_en_US_1X._CB451228332_.jpg' alt='' />
+        </ProductSlider>
+        <Footer />
+      </S.Home>
   )
 }
 
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -4,3 +4,45 @@ export const Home = styled.div`
   width: 100%;
   background: #EAEDED ; 
 `;
+
+export const SideMenuContainer = styled.div`
+  display:block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  visibility: ${props => props.visible ? 'visible' : 'hidden'};
+  z-index: 1;
+`;
+export const DarkScreen = styled.div`
+  background-color: rgba(0, 0, 0, .8);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: ${props => props.visible ? .9 : 0};
+  transition:  .3s;
+`;
+
+export const CloseIcon = styled.div`
+  background-image:url('https://images-na.ssl-images-amazon.com/images/G/01/gno/sprites/nav-sprite-global-1x-hm-dsk-reorg._CB405937547_.png');
+  background-position: -190px -288px;
+  position: fixed;
+  left: 380px;
+  top: 20px;
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+`;
+
+export const SideMenu = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 365px;
+  background-color: white;
+  box-shadow: 4px 0 10px 0 rgba(0,0,0,.4);
+  transition: .3s;
+  transform: translateX(${props => props.visible ? ('0') : ('-100%')});
+  `;
+
+export const SideHeader = styled.div``;
