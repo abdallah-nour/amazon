9bf2144a09c12730159744ead5f937dc19f0cf95
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-12-24 23:02:23 +0200

    not-important

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 // import Header from './components/Header';
 {/* <link href="//db.onlinewebfonts.com/c/157c6cc36dd65b1b2adc9e7f3329c761?family=Amazon+Ember" rel="stylesheet" type="text/css" /> */ }
-import { useState, useEffect } from 'react';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Search from './pages/Search';
-import Cart from './pages/Cart';
-import { Wrapper, Container } from './components/Container';
-import { StateProvider } from './components/StateProvider';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { auth } from './firebase';
 import './App.css';
-import reducer, { initState } from './components/reducer';
+
+import { Container, Wrapper } from './components/Container';
 import {
+  Route,
   BrowserRouter as Router,
   Switch,
-  Route,
 } from "react-router-dom";
+import reducer, { initState } from './components/reducer';
+import { useEffect, useState } from 'react';
+
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Search from './pages/Search';
 import SideMenu from './components/SideMenu';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import { StateProvider } from './components/StateProvider';
+import { auth } from './firebase';
 
 function App() {
   const [visibleSideMenu, setVisibility] = useState(false);
   useEffect(() => {
+    const usr = auth.currentUser;
+    if(usr){
+      console.log('there is user/ ', usr);
+    }else{
+      console.log("there isn't any ");
+    }
     auth.onAuthStateChanged((user) => {
       console.log('The user is >>> ', user);
       if (user) {
