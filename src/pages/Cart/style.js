d8fbf85b0e2f516b06e6e8feb38fefc52fbaeb9b
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-12-24 22:56:49 +0200

    cart-finished

--- src/pages/Cart/style.js
+++ src/pages/Cart/style.js
@@ -1,24 +1,42 @@
 import styled from 'styled-components';
 
+const mobileWidth = '1070px';
+
 export const Container = styled.div`
   padding:30px 18px 18px;
   background-color:#EAEDED;
+  min-height: 100vh;
   display:flex;
   justify-content: space-between;
+  @media only screen and (max-width:${mobileWidth}){
+    justify-content: flex-end;
+    flex-direction:column-reverse;
+  }
 `;
 export const Left = styled.div`
-  width: 100%;
-  max-width: 985px;
+  width: 70%;
+  margin:0 auto;
+  @media only screen and (max-width: ${mobileWidth}){
+    width: 90%;
+  }
 `;
 
 export const Right = styled.div`
-  width: 100%;
+  width: 39%;
+  margin: 0 auto;
   max-width: 350px;
+  @media only screen and (max-width: ${mobileWidth}){
+    background-color: white;
+    width: 90%;
+    max-width: unset;
+  }
 `;
 
 export const RightContent = styled.div`
   background-color: white;
   padding: 20px;
+  margin: 0 auto;
+  max-width: 390px;
 `;
 
 export const CheckBoxLabel = styled.label`
